62b19484c3f1d2ab4dce9543fc490f0dfb31e241
daniel-wang <daniel_wsy@163.com>
2018-11-29 18:00:16 +0800

    ViewModel注释

--- app/src/main/java/com/wheel/daniel/okhttputils/viewmodel/GankModel.java
+++ app/src/main/java/com/wheel/daniel/okhttputils/viewmodel/GankModel.java
@@ -14,6 +14,9 @@ import com.wheel.daniel.okhttputils.utils.ThreadMgrUtils;
  * @author danielwang
  * @Description: 获取图片信息，装载到LivaData中
  * @date 2018/9/11 14:07
+ * ViewModel 的存在是依赖 Activity 或者 Fragment的，
+ * 不管你在什么地方获取ViewModel ，只要你用的是相同的Activity或者 Fragment，
+ * 那么获取到的ViewModel将是同一个 (前提是key值是一样的)，所以ViewModel 也具有数据共享的作用！
  */
 public class GankModel extends ViewModel {
 
